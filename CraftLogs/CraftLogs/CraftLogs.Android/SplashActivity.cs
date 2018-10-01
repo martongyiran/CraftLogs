8b29d14eb4871e115960c658e1c500b0c2e9b23d
Marton Gyiran <gyiran.marton@infoframe.hu>
2018-10-01 15:56:07 +0200

    Add new icon and create splashScreen

---
+++ CraftLogs/CraftLogs/CraftLogs.Android/SplashActivity.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.OS;
+
+namespace CraftLogs.Droid
+{
+    [Activity(Label = "CraftLogs", Theme = "@style/Theme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            StartActivity(typeof(MainActivity));
+            // Create your application here
+        }
+    }
+}
