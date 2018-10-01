ad46e30ed1fc5a416a860a30f4ffd25a76317be6
Marton Gyiran <gyiran.marton@infoframe.hu>
2018-10-01 16:36:34 +0200

    Comments added: DataService

--- CraftLogs/CraftLogs/CraftLogs.Services/DataService.cs
+++ CraftLogs/CraftLogs/CraftLogs.Services/DataService.cs
@@ -4,11 +4,31 @@ namespace CraftLogs.Services
 {
     public static class DataService
     {
+        #region Private functions
+        private static string GetPath()
+        {
+            var tempPath = Path.GetTempPath();
+            var path = tempPath.Replace("/cache/", "");
+            path += string.Format("/{0}", "files");
+            return path;
+        }
+        
+        private static string GetFilePath(string fileName)
+        {
+            return Path.Combine(GetPath(), fileName);
+        }
+        #endregion
+
+        #region Public functions
+        /// <summary> Verifies that the specified file already exists. </summary>
+        /// <param name="fileName">File name.</param>
         public static bool IsFileExist(this string fileName)
         {
             return File.Exists(GetFilePath(fileName));
         }
 
+        /// <summary> Create a new file. </summary>
+        /// <param name="fileName">File name.</param>
         public static bool CreateFile(this string fileName)
         {
             if (!fileName.IsFileExist())
@@ -19,11 +39,16 @@ namespace CraftLogs.Services
             return false;
         }
 
+        /// <summary> Writes all text to the given file. </summary>
+        /// <param name="fileName">File name.</param>
+        /// <param name="content">The text that we want to write to the file.</param>
         public static void WriteAllText(this string fileName, string content = "")
         {
             File.WriteAllText(GetFilePath(fileName), content);
         }
 
+        /// <summary> Reads all text from the given file. </summary>
+        /// <param name="fileName">File name.</param>
         public static string ReadAllText(this string fileName)
         {
             string content = "";
@@ -34,22 +59,12 @@ namespace CraftLogs.Services
             return content;
         }
 
+        /// <summary> Delete a file. </summary>
+        /// <param name="fileName">File name.</param>
         public static void DeleteFile(string fileName)
         {
             File.Delete(GetFilePath(fileName));
         }
-
-        private static string GetPath()
-        {
-            var tempPath = Path.GetTempPath();
-            var path = tempPath.Replace("/cache/", "");
-            path += string.Format("/{0}", "files");
-            return path;
-        }
-
-        private static string GetFilePath(string fileName)
-        {
-            return Path.Combine(GetPath(), fileName);
-        }
+        #endregion
     }
 }
