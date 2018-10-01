ac895170a9ff6c85ba73f5a6debd77fb3fbd7dd8
Marton Gyiran <gyiran.marton@infoframe.hu>
2018-10-01 15:59:31 +0200

    Clean.bat updated

--- CraftLogs/CraftLogs/Clean.bat
+++ CraftLogs/CraftLogs/Clean.bat
@@ -4,4 +4,16 @@ RD /s /q %CD%\CraftLogs\obj
 RD /s /q %CD%\CraftLogs.Android\bin
 RD /s /q %CD%\CraftLogs.Android\obj
 
+RD /s /q %CD%\CraftLogs.Views\bin
+RD /s /q %CD%\CraftLogs.Views\obj
+
+RD /s /q %CD%\CraftLogs.ViewModels\bin
+RD /s /q %CD%\CraftLogs.ViewModels\obj
+
+RD /s /q %CD%\CraftLogs.Values\bin
+RD /s /q %CD%\CraftLogs.Values\obj
+
+RD /s /q %CD%\CraftLogs.Services\bin
+RD /s /q %CD%\CraftLogs.Services\obj
+
 pause
