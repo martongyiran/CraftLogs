b8ec2be0e604ec9ef76103526efebb8ae7110533
Márton Áron Gyirán <gyiranmarton@gmail.com>
2018-10-04 17:01:27 +0200

    Settings UI rework

--- CraftLogs/CraftLogs/CraftLogs.Values/Texts.cs
+++ CraftLogs/CraftLogs/CraftLogs.Values/Texts.cs
@@ -10,5 +10,56 @@
         /// Version number on MainPage with {0} args.
         /// </summary>
         public const string Version = "Version: {0}";
+
+        #region SettingsPage texts 
+        /// <summary>
+        /// SettingsPage toolbar text.
+        /// </summary>
+        public const string ToDefault = "Alaphelyzet";
+        /// <summary>
+        /// SettingsPage list text.
+        /// </summary>
+        public const string CraftDay = "Craft nap: ";
+        /// <summary>
+        /// SettingsPage list text.
+        /// </summary>
+        public const string Craft1Start = "Craft 1 kezdete(óra 0-24) ";
+        /// <summary>
+        /// SettingsPage list text.
+        /// </summary>
+        public const string Craft2Start = "Craft 2 kezdete(óra 0-24) ";
+        /// <summary>
+        /// SettingsPage list text.
+        /// </summary>
+        public const string Craft1MinPont = "Craft 1 Minimum Pont ";
+        /// <summary>
+        /// SettingsPage list text.
+        /// </summary>
+        public const string Craft2MinPont = "Craft 2 Minimum Pont ";
+        /// <summary>
+        /// SettingsPage list text.
+        /// </summary>
+        public const string Craft1QuestCount = "Craft 1 állomások száma ";
+        /// <summary>
+        /// SettingsPage list text.
+        /// </summary>
+        public const string Craft2QuestCount = "Craft 2 állomások száma ";
+        /// <summary>
+        /// SettingsPage: save settings text.
+        /// </summary>
+        public const string SaveSettings = "Mentés";
+        /// <summary>
+        /// SettingsPage: sum text.
+        /// </summary>
+        public const string Sum = "Összegzés";
+        /// <summary>
+        /// SettingsPage: delete profile text.
+        /// </summary>
+        public const string DeleteProfile = "Profil törlése";
+        /// <summary>
+        /// SettingsPage: GodMode text.
+        /// </summary>
+        public const string GodMode = "Isteni üzemmód";
+        #endregion
     }
 }
--- CraftLogs/CraftLogs/CraftLogs.ViewModels/SettingsPageViewModel.cs
+++ CraftLogs/CraftLogs/CraftLogs.ViewModels/SettingsPageViewModel.cs
@@ -6,9 +6,85 @@ namespace CraftLogs.ViewModels
 {
     public class SettingsPageViewModel : ViewModelBase
     {
+        #region Properties
+
+        private int craftDay;
+
+        public int CraftDay
+        {
+            get { return craftDay; }
+            set { SetProperty(ref craftDay, value); }
+        }
+
+        private int craft1Start;
+
+        public int Craft1Start
+        {
+            get { return craft1Start; }
+            set { SetProperty(ref craft1Start, value); }
+        }
+
+        private int craft2Start;
+
+        public int Craft2Start
+        {
+            get { return craft2Start; }
+            set { SetProperty(ref craft2Start, value); }
+        }
+
+        private int craft1MinPont;
+
+        public int Craft1MinPont
+        {
+            get { return craft1MinPont; }
+            set { SetProperty(ref craft1MinPont, value); }
+        }
+
+        private int craft2MinPont;
+
+        public int Craft2MinPont
+        {
+            get { return craft2MinPont; }
+            set { SetProperty(ref craft2MinPont, value); }
+        }
+
+        private int craft1QuestCount;
+
+        public int Craft1QuestCount
+        {
+            get { return craft1QuestCount; }
+            set { SetProperty(ref craft1QuestCount, value); }
+        }
+
+        private int craft2QuestCount;
+
+        public int Craft2QuestCount
+        {
+            get { return craft2QuestCount; }
+            set { SetProperty(ref craft2QuestCount, value); }
+        }
+
+        #endregion
         public SettingsPageViewModel(INavigationService navigationService, ILocalDataRepository dataRepository) : base(navigationService, dataRepository)
         {
             Title = Texts.SettingsPage;
+        }
+
+        public override void OnNavigatedTo(NavigationParameters parameters)
+        {
+            base.OnNavigatedTo(parameters);
+            SetUp();
+        }
+
+        private void SetUp()
+        {
+            CraftDay = 123;
+            Craft1Start = 456;
+            Craft2Start = 789;
+            Craft1MinPont = 101;
+            Craft2MinPont = 112;
+            Craft1QuestCount = 131;
+            Craft2QuestCount = 415;
         }
     }
 }
