4860aef41c45b1d813b1014c81f40a9aeb0dc9f2
Gyirán Márton Áron <gyiranmarton@gmail.com>
2020-03-30 19:15:33 +0200

    Converters for ItemView

---
+++ CraftLogs/CraftLogs/CraftLogs.Views/Converters/CharacterClassEnumToCharConverter.cs
@@ -0,0 +1,33 @@
+﻿using CraftLogs.BLL.Enums;
+using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace CraftLogs.Views.Converters
+{
+    public class CharacterClassEnumToCharConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is CharacterClassEnum)
+            {
+                return value switch
+                {
+                    CharacterClassEnum.Mage => "M",
+                    CharacterClassEnum.Rogue => "R",
+                    CharacterClassEnum.Warrior => "W",
+                    _ => "-",
+                };
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CraftLogs/CraftLogs/CraftLogs.Views/Converters/ItemRarityToColorConverter.cs
@@ -0,0 +1,33 @@
+﻿using CraftLogs.BLL.Enums;
+using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace CraftLogs.Views.Converters
+{
+    public class ItemRarityToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is ItemRarityEnum)
+            {
+                return value switch
+                {
+                    ItemRarityEnum.Common => Color.Black,
+                    ItemRarityEnum.Rare => Color.Blue,
+                    ItemRarityEnum.Legendary => Color.Orange,
+                    _ => Color.White,
+                };
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
